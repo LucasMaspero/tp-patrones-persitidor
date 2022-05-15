7711db5645c64b40864cf03d988649132db16fee
Lucas Maspero <masperolucas@gmail.com>
2022-05-15 19:08:47 -0300

    avances

--- workspace/tp-patrones-persistidor/src/main/java/persistidor/anotaciones/Persistable.java
+++ workspace/tp-patrones-persistidor/src/main/java/persistidor/anotaciones/Persistable.java
@@ -1,5 +1,7 @@
 package persistidor.anotaciones;
 
+// Buscar como definir que la anotacion puedar ir a nivel clase, a nivel metodo o a nivel propiedad
+// Necesitamos que sea a nivel clase y a nivel propiedad
 public @interface Persistable 
 {
 }
